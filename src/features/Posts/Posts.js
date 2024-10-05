ffb34bb2f0af16720e1749785fa50fb20bbf04bb
Tommy Gee <100529843+tommygee63@users.noreply.github.com>
2024-10-05 22:43:17 +0100

    missed staged save for loading logic

--- src/features/Posts/Posts.js
+++ src/features/Posts/Posts.js
@@ -23,12 +23,6 @@ export function Posts() {
         )
     }
 
-   /* if (postsData.posts.isPending) {
-        return (
-            
-        )
-    }*/
-
     return (
         <>
         {postsData.isPending ? <div className={styles.postsDiv}>
